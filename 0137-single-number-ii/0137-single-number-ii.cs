27e2f86809d9911008bec39f0f6f1a76b75b506e
Naveen Thomas <naveenthomasabc123@gmail.com>
2025-05-07 11:16:02 +0530

    Time: 152 ms (5.67%), Space: 44.8 MB (6.07%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.cs
+++ 0137-single-number-ii/0137-single-number-ii.cs
@@ -1,25 +1,8 @@
+using System.Linq;
+
 public class Solution {
     public int SingleNumber(int[] nums) {
-        if (nums.Length == 1) return nums[0];
-
-        Array.Sort(nums);
-        int value = nums[0];
-        int count = 1;
-
-        for (int i = 1; i < nums.Length; i++) {
-            if (nums[i] == nums[i - 1]) {
-                count++;
-            } else {
-                if (count == 1) {
-                    return nums[i - 1];
-                }
-                value = nums[i];
-                count = 1;
-            }
-        }
-        if (count == 1) {
-            return value;
-        }
-        return value; 
+        return nums.Distinct()
+                   .First(num => nums.Count(x => x == num) == 1); 
     }
 }
